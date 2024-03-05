a2b0abcd4ebc82c9a98cbcfce0ff1a5cab5505cc
Raphael Rösch <raphael.roesch@1und1.de>
2024-03-05 14:38:41 -0500

    Add missed import for `@Qualifier` in the `ObservationTests`

--- spring-kafka/src/test/java/org/springframework/kafka/support/micrometer/ObservationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/support/micrometer/ObservationTests.java
@@ -40,6 +40,7 @@ import org.apache.kafka.common.header.Headers;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.KafkaException;
