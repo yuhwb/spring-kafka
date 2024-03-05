cccbd44b53943979b44db702ea49ead7ff010659
Raphael Rösch <raphael.roesch@1und1.de>
2024-03-05 20:38:41 +0100

    GH-3076: Set `batchFailed` in `ListenerConsumer` even if no `commonErrorHandler`
    Fixes: #3076
    
    The `ListenerConsumer` sets `batchFailed` to `true` only if `commonErrorHandler` is provided
    
    * Move `this.batchFailed = true;` in the `doInvokeBatchListener()` before any error handling.
    
    # Conflicts:
    #	spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
    #	spring-kafka/src/test/java/org/springframework/kafka/listener/TransactionalContainerTests.java
    
    (cherry picked from commit e4d9994a0396ac3871151ff0e98e5a9ea89c8dd5)

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -160,6 +160,7 @@ import io.micrometer.observation.ObservationRegistry;
  * @author Francois Rosiere
  * @author Daniel Gentes
  * @author Soby Chacko
+ * @author Raphael Rösch
  */
 public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 		extends AbstractMessageListenerContainer<K, V> implements ConsumerPauseResumeEventPublisher {
@@ -2313,13 +2314,13 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 				}
 			}
 			catch (RuntimeException e) {
+				this.batchFailed = true;
 				failureTimer(sample, null);
 				batchInterceptAfter(records, e);
 				if (this.commonErrorHandler == null) {
 					throw e;
 				}
 				try {
-					this.batchFailed = true;
 					invokeBatchErrorHandler(records, recordList, e);
 					commitOffsetsIfNeededAfterHandlingError(records);
 				}
