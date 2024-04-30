572d13a50c63aaf86c9e8c01c9e6b0ed75b783c7
Soby Chacko <soby.chacko@broadcom.com>
2024-04-30 14:59:23 -0400

    Adding DirtiesContext to WithNestedClassContextTests
    * To prevent a race condition on shutdown that is causing certain file operations to fail,
      we need to add `DirtiesContext` to `WithNestedClassContextTests.`

--- spring-kafka-test/src/test/java/org/springframework/kafka/test/condition/WithNestedClassContextTests.java
+++ spring-kafka-test/src/test/java/org/springframework/kafka/test/condition/WithNestedClassContextTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 the original author or authors.
+ * Copyright 2021-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,10 +28,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+/**
+ * @author Lee Jaeheon
+ * @author Soby Chacko
+ */
 @EmbeddedKafka
 @SpringJUnitConfig(WithNestedClassContextTests.Config.class)
+@DirtiesContext
 class WithNestedClassContextTests {
 
 	private static final AtomicInteger counter = new AtomicInteger();
@@ -56,17 +62,20 @@ class WithNestedClassContextTests {
 		void equalsCount() {
 			assertThat(counter.get()).isEqualTo(1);
 		}
+
 	}
 
-	public static class TestClass {
-	}
+	public static class TestClass { }
 
 	@Configuration
 	static class Config {
+
 		@Bean
 		public TestClass testClass() {
 			counter.incrementAndGet();
 			return new TestClass();
 		}
+
 	}
+
 }
