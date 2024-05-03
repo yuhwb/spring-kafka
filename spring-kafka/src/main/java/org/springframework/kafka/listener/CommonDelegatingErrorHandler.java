18e509ec01bd33d24575368ce0cea77fd24d756a
Dan Blackney <danblackney@gmail.com>
2024-05-03 22:28:22 +0800

    GH-3227: Implement handleOne() in CDEH
    Fixes: #3227
    
    * Implement handleOne() in `CommonDelegatingErrorHandler`
    * Add tests for handle methods in `CommonDelegatingErrorHandler`
    * Add tests for the new `handleOne()` method, as well as a test for `handleOtherException()`
    * Checkstyle fixes
    
    (cherry picked from commit 4e06c2c16554159179eea316c9089f1e95d85a54)

--- spring-kafka/src/main/java/org/springframework/kafka/listener/CommonDelegatingErrorHandler.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/CommonDelegatingErrorHandler.java
@@ -38,6 +38,8 @@ import org.springframework.util.Assert;
  *
  * @author Gary Russell
  * @author Adrian Chlebosz
+ * @author Antonin Arquey
+ * @author Dan Blackney
  * @since 2.8
  *
  */
@@ -185,6 +187,19 @@ public class CommonDelegatingErrorHandler implements CommonErrorHandler {
 		}
 		else {
 			this.defaultErrorHandler.handleOtherException(thrownException, consumer, container, batchListener);
+		}
+	}
+
+	@Override
+	public boolean handleOne(Exception thrownException, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer,
+			MessageListenerContainer container) {
+
+		CommonErrorHandler handler = findDelegate(thrownException);
+		if (handler != null) {
+			return handler.handleOne(thrownException, record, consumer, container);
+		}
+		else {
+			return this.defaultErrorHandler.handleOne(thrownException, record, consumer, container);
 		}
 	}
 
