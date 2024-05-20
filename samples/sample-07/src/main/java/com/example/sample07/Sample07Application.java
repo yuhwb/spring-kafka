00fd11b51cacc1e64e2755dddee9e0bbaecb69ee
ChickenchickenLove <ojt90902@naver.com>
2024-05-21 00:03:20 +0900

    GH-3145 : Sample for next gen consumer-group rebalance protocol
    Fixes: #3145 
    
    * Add a sample demonstrating the next-generation consumer-group rebalance protocol.
    * Provide a docker-compose script for spinning up a `kraft` based Kafka broker since the new consumer 
      protocol only works in `kraft` mode. 
    * Add README explaining the sample.

---
+++ samples/sample-07/src/main/java/com/example/sample07/Sample07Application.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.sample07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+/**
+ * New consumer rebalance protocol sample which purpose is only to demonstrate the application
+ * of the New Consumer Rebalance Protocol in Spring Kafka.
+ *
+ * @author Sanghyeok An.
+ *
+ * @since 3.3
+ */
+
+@EnableKafka
+@SpringBootApplication
+public class Sample07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample07Application.class, args);
+	}
+
+}
